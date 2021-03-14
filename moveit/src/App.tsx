0a0b32d1b6aaeb9a787a3031c625658f1c3903f1
Gustavo Lima <gustavo.lima@liferay.com>
2021-03-14 14:24:04 -0300

    Add nlw project

---
+++ moveit/src/App.tsx
@@ -0,0 +1,21 @@
+import { Button } from './components/Button'
+
+import './styles';
+
+function App() {
+  return (
+    <div>
+      <Button color="red"> 
+        Button 1
+      </Button>
+      <Button color="blue"> 
+        Button 2
+      </Button>
+      <Button color="green"> 
+        Button 3
+      </Button>
+    </div>
+  );
+}
+
+export default App;
