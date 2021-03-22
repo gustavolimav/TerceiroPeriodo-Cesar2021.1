9fcea97ee09c16541bf74ecb199a415593dc501b
Gustavo Lima <gustavo.lima@liferay.com>
2021-03-22 15:33:44 -0300

    Update

--- infraDeSoftware/1imp/glvm/pthread/test1.c
+++ infraDeSoftware/1imp/glvm/pthread/test1.c
@@ -12,11 +12,14 @@
 #include <sys/mman.h>
 
 int sum; /* esses dados são compartilhados pelo(s) thread(s) */
-void *runner(int l, int c, void *param); /* os threads chamam essa função */
+void *runner(void *param); /* os threads chamam essa função */
 int main(int argc, char *argv[])
 {
     pthread_t tid; /* o identificador do thread */
     pthread_attr_t attr; /* conjunto de atributos do thread */
+    FILE *fp;
+    char texto_str[4000];
+    
     if (argc != 2) {
         fprintf(stderr,"usage: a.out <integer value>\n");
         return -1;
@@ -28,20 +31,22 @@ int main(int argc, char *argv[])
     /* obtém os atributos default */
     pthread_attr_init(&attr);
     /* cria o thread */
-    int fst[2][2] = {1,1,1,1};
-    int fst[2][2] = {2,2,2,2};
-    int fst[4][4];
 
-    pthread_create(&tid,&attr,runner,2,2,fst);
+    pthread_create(&tid,&attr,runner,argv[1]);
     /* espera o thread ser encerrado */
+    fp = fopen(argv[1], "w");
+    while(fgets(texto_str, 20, fp) != NULL)
+    printf("%s", texto_str);
+    fclose(fp);
     pthread_join(tid,NULL);
     printf("sum = %d\n",sum);
 }
 /* O thread assumirá o controle nessa função */
-void *runner(int l, int c, void *param) {
-    int i, upper = atoi(param);
-    sum = 0;
-    for (i = 1; i <= upper; i++)
-        sum += upper;
+void *runner(void *param) {
+    FILE *fp;
+    char texto_str[4000];
+
+    fp = fopen(param, "w");
+    fclose(fp);
     pthread_exit(0);
 }
