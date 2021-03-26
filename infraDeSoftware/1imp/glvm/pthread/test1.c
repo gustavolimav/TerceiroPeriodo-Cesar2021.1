a9ced3a55e86d0e1104a08c4f634746591d035df
Gustavo Lima <gustavolvma@gmail.com>
2021-03-26 09:46:20 -0300

    Update

--- infraDeSoftware/1imp/glvm/pthread/test1.c
+++ infraDeSoftware/1imp/glvm/pthread/test1.c
@@ -12,60 +12,51 @@
 #include <sys/mman.h>
 
 int sum; /* esses dados são compartilhados pelo(s) thread(s) */
-<<<<<<< HEAD
-void *runner(void* fst); /* os threads chamam essa função */
-=======
-void *runner(void *param); /* os threads chamam essa função */
->>>>>>> abe0456efc80ee971359f522ad4ce98c7df82ca1
+void *runner(void* param); /* os threads chamam essa função */
 int main(int argc, char *argv[])
 {
     pthread_t tid; /* o identificador do thread */
     pthread_attr_t attr; /* conjunto de atributos do thread */
     FILE *fp;
     char texto_str[4000];
-    
-    if (argc != 2) {
-        fprintf(stderr,"usage: a.out <integer value>\n");
-        return -1;
-    }
-    if (atoi(argv[1]) < 0) {
-        fprintf(stderr,"%d must be >= 0\n",atoi(argv[1]));
-        return -1;
-    }
-    /* obtém os atributos default */
-    pthread_attr_init(&attr);
-    /* cria o thread */
-<<<<<<< HEAD
-    int fst[2][2] = {2,2,2,2};
-    int* a = malloc((sizeof(int)));
-    *a = fst[];
-    pthread_create(&tid,&attr,runner,a);
-=======
+    int n = 0;
 
-    pthread_create(&tid,&attr,runner,argv[1]);
->>>>>>> abe0456efc80ee971359f522ad4ce98c7df82ca1
-    /* espera o thread ser encerrado */
-    fp = fopen(argv[1], "w");
-    while(fgets(texto_str, 20, fp) != NULL)
-    printf("%s", texto_str);
-    fclose(fp);
+    pthread_attr_init(&attr);
+    for (int i = 0; i < 4; i++) {
+        fp = fopen("COUNT", "w");
+        n++;
+        fprintf(fp, "%d", n);
+        fclose(fp);
+        fp = fopen("COUNT", "w");
+        fscanf(fp, "%d", &n);
+        printf("tid1 %d\n", n);
+        fclose(fp);
+        pthread_create(&tid,&attr,runner,argv[1]);
+    }
     pthread_join(tid,NULL);
     printf("sum = %d\n",sum);
 }
 /* O thread assumirá o controle nessa função */
-<<<<<<< HEAD
-void *runner(void* fst) {
-    int i, upper = atoi(fst);
-    sum = 0;
-    for (i = 1; i <= upper; i++)
-        sum += upper;
-=======
 void *runner(void *param) {
-    FILE *fp;
-    char texto_str[4000];
+    FILE *fp, *fp2, *fp3, *count;
 
-    fp = fopen(param, "w");
+    fp = fopen("m1.txt", "w");
+    fp2 = fopen("m2.txt", "w");
+    fp3 = fopen("m3.txt", "w");
+    count = fopen("COUNT", "r");
+    int n;
+
+    
+	fscanf(count, "%d", &n);
+    printf("tid %d\n", n);
+    for (int a = 0; a < 2; a++) {
+		for (int b = 0; b < 2; b++)
+			fprintf(fp3, "%d ", 1);
+		fprintf(fp3, "%s", "\n");
+	}
+    fclose(fp3);
     fclose(fp);
->>>>>>> abe0456efc80ee971359f522ad4ce98c7df82ca1
+    fclose(fp2);
+    fclose(count);
     pthread_exit(0);
 }
