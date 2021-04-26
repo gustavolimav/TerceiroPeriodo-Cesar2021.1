b02d9ec5e7c73cfccd8e98b306a23de71b241ead
Gustavo Lima <gustavolvma@gmail.com>
2021-04-26 17:18:16 -0300

    Update

---
+++ poo2021.1/alura/java_test_Junit/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -0,0 +1,36 @@
+package br.com.alura.tdd.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.tdd.modelo.Funcionario;
+
+public class BonusServiceTest {
+    
+    @Test
+    void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
+        
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario
+        ("Diogo", LocalDate.now(), new BigDecimal("25000")));
+
+        assertEquals(new BigDecimal("0.00"), bonus);
+    }
+
+    @Test
+    void bonusDeveriaSer10PorCentoDoSalario() {
+
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario
+        ("Diogo", LocalDate.now(), new BigDecimal("2000")));
+        
+        assertEquals(new BigDecimal("200.00"), bonus);
+
+    }
+}
