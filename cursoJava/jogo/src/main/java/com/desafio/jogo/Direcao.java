e4c77b4b2f77308a57354dc299ef38b66dd5166f
Gustavo Lima <gustavolvma@gmail.com>
2021-05-19 15:30:12 -0300

    Update

---
+++ cursoJava/jogo/src/main/java/com/desafio/jogo/Direcao.java
@@ -0,0 +1,5 @@
+package com.desafio.jogo;
+
+public enum Direcao {
+    NORTE, LESTE, SUL, OESTE
+}
---
+++ cursoJava/jogo/src/main/java/com/desafio/jogo/Jogo.java
@@ -0,0 +1,17 @@
+package com.desafio.jogo;
+
+/**
+ * Hello world!
+ *
+ */
+public class Jogo 
+{
+    public static void main( String[] args )
+    {
+        Jogador j1 = new Jogador();
+        j1.x = 10;
+        j1.y = 20;
+
+        
+    }
+}
